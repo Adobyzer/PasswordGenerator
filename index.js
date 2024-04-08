be08790689f6adcd13e6784aa76a2be2baef7062
Adobyzer <adam.abed02@hotmail.com>
2024-04-08 01:16:43 -0400

    Moduling and refractoring the previous functionnality

--- index.js
+++ index.js
@@ -5,36 +5,47 @@ const symbolsOff = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 
 const numbersOff = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-const body = document.getElementById("body")
 
 const lengthPassword = document.getElementById("length")
 
-const lengthPasswordLabel = document.getElementById("lengthLabel")
-
-
 const button = document.getElementById("generate")
 
-const textfieldLeft = document.getElementById("text-left")
+//list of functions 
 
-const textfieldRight = document.getElementById("text-right")
-
-
-button.addEventListener("click",function(){
-    let sumCaracter1 = ""
+function generatePassword(){
+  let sumCaracter1 = ""
     let sumCaracter2 = ""
     
+    //concatenate terms on it
     for (let i = 0 ; i < lengthPassword.value ; i++){
         sumCaracter1 +=  characters[Math.floor(Math.random()*characters.length)]
         sumCaracter2 +=  characters[Math.floor(Math.random()*characters.length)]
     }
    
+  const textfieldLeft = document.getElementById("text-left")
+
+  const textfieldRight = document.getElementById("text-right")
     
+    //display to option for better
     textfieldLeft.value = sumCaracter1
     textfieldRight.value = sumCaracter2
+ 
+}
+
+function displayLenghtPassword(event){
+  const lengthPasswordLabel = document.getElementById("lengthLabel")
+
+  lengthPasswordLabel.textContent =  " Length password : " +event.target.value;
+}
+
+
+//list of event listener in app
+button.addEventListener("click",() =>{
+  generatePassword()
 })
 
 
-lengthPassword.addEventListener("input", (event) => {
-  lengthPasswordLabel.textContent =  " Length password : " +event.target.value;
-});
 
+lengthPassword.addEventListener("input", (event) => {
+ displayLenghtPassword(event)
+})
